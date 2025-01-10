5cba80f2bf50ffc130da8f2409972ba089623954
retif <retif@decovar.dev>
2025-01-10 12:11:01 +0100

    Docker: Moved download folder from script to yt-dlp config

--- docker/images/yt-dlp/ydl.sh
+++ docker/images/yt-dlp/ydl.sh
@@ -3,7 +3,7 @@
 tmpfile=$(mktemp)
 
 echo "[$(date +%Y-%m-%d-%H-%M-%S)] Requested a download of: $1" > $tmpfile
-yt-dlp $1 -P home:~/downloads/ 2>>$tmpfile
+yt-dlp $1 2>>$tmpfile
 echo "" >> $tmpfile && echo "---" >> $tmpfile && echo "" >> $tmpfile
 
 cat $tmpfile >> ~/downloads/requested-urls.log && rm $tmpfile
